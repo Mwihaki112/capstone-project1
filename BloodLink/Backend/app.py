16a95bfef4ec944ee990af51039ffa6c8ae03981
Agnes Mwihaki <moringa@DESKTOP-89EPAHO.localdomain>
2026-08-13 15:24:44 +0300

    Add missing JWT_SECRET_KEY

--- BloodLink/Backend/app.py
+++ BloodLink/Backend/app.py
@@ -23,7 +23,7 @@ CORS(
     origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
-        "https://capstone-project-1-fw4z.onrender.com"
+        "https://bloodlink-frontend-eru4.onrender.com"
     ]
 )
 
